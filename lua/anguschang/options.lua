c429f85aefe6e4d42b8bf62be1e8ddb9f234f2ce
Angus Chang <changangus2@gmail.com>
2023-03-26 22:15:41 -0400

    feat: telescope and basic options

---
+++ lua/anguschang/options.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt
+local cmd = vim.cmd
+
+-- Options:
+opt.scrolloff = 8
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.undofile = true
+
+cmd('colorscheme catppuccin')
+
