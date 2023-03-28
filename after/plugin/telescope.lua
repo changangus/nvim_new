78fb518958750db55efb5c85541e043d3f09cded
Angus Chang <changangus2@gmail.com>
2023-03-27 22:59:52 -0400

    feat: remap lsp definition to use telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -21,10 +21,10 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>a', function() builtin.diagnostics({ bufnr = 0 }) end)
 vim.keymap.set('n', 'gr', function() builtin.lsp_references() end)
+vim.keymap.set('n', 'gd', function() builtin.lsp_definitions() end)
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fe', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
-
 
