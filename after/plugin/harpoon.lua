b1dc5a03ec7cb2cba525b4d01d339cf1eb175610
Angus Chang <changangus2@gmail.com>
2023-03-27 22:17:07 -0400

    feat: harpoon setup

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,11 @@
+-- Harpoon
+require("harpoon").setup({
+  menu = {
+    width = 110,
+    height = 20,
+  }
+})
+vim.keymap.set('n', '<leader>lv', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>la', ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>ht', ':Telescope harpoon marks<CR>', { noremap = true })
+
