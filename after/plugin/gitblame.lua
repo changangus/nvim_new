ffcbd3ab860401ce2bebac9ac38b96c3eb8927ca
Angus Chang <changangus2@gmail.com>
2023-03-27 22:20:34 -0400

    feat: gitblame setup

---
+++ after/plugin/gitblame.lua
@@ -0,0 +1,5 @@
+-- GitBlame
+vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<CR>', { noremap = true });
+vim.keymap.set('n', '<leader>gO', ':GitBlameOpenCommitURL<CR>', { noremap = true });
+vim.keymap.set('n', '<leader>gS', ':GitBlameCopySHA<CR>', { noremap = true });
+
