84dbd7dd1ed11a35d88f2c333b5e5bb77ea47d23
Angus Chang <changangus2@gmail.com>
2023-03-27 20:56:38 -0400

    feat: more options and keymaps

--- lua/anguschang/options.lua
+++ lua/anguschang/options.lua
@@ -1,12 +1,18 @@
-local opt = vim.opt
-local cmd = vim.cmd
-
 -- Options:
-opt.scrolloff = 8
-opt.number = true
-opt.relativenumber = true
-opt.mouse = "a"
-opt.undofile = true
+vim.opt.scrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.undofile = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.updatetime = 300
 
-cmd('colorscheme catppuccin')
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
 
--- lua/anguschang/remaps.lua
+++ lua/anguschang/remaps.lua
@@ -18,10 +18,10 @@ map('n', '<leader>nh', ':noh<CR>', { noremap = true })
 map('x', '<leader>y', '"+y', { noremap = true })
 map('n', '<leader>rp', ':let @+ = expand("%")<CR>', { noremap = true })
 -- Move lines up and down
-map('n', '<S-j>', ':.move +1<CR>')
-map('n', '<S-k>', ':.move -2<CR>')
-map('v', '<S-j>', ":'<,'>move '>+1 | normal! gv<CR>")
-map('v', '<S-k>', ":'<,'>move '<-2 | normal! gv<CR>")
+map('n', '<S-d>', ':.move +1<CR>')
+map('n', '<S-u>', ':.move -2<CR>')
+map('v', '<S-d>', ":'<,'>move '>+1 | normal! gv<CR>")
+map('v', '<S-u>', ":'<,'>move '<-2 | normal! gv<CR>")
 -- Split screen 50% vertically 
 map('n', '<leader>v', ':vsplit<CR>:resize 50<CR>', { noremap = true })
 -- Split screen 10% horizontally
