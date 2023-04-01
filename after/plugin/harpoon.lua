636951c6e4112f5bd1112b0309b4c6b9016369e6
Angus Chang <changangus2@gmail.com>
2023-03-31 20:00:23 -0400

    feat: style harpoon gui

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,7 +5,17 @@ require("harpoon").setup({
     height = 20,
   }
 })
+
 vim.keymap.set('n', '<leader>lv', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>la', ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
 vim.keymap.set('n', '<leader>ht', ':Telescope harpoon marks<CR>', { noremap = true })
+
+vim.api.nvim_create_autocmd({ "Filetype" }, {
+  pattern = "harpoon",
+  callback = function()
+    vim.opt.cursorline = true
+    vim.api.nvim_set_hl(0, 'HarpoonWindow', { link = 'Normal' })
+    vim.api.nvim_set_hl(0, 'HarpoonBorder', { link = 'Normal' })
+  end
+})
 
