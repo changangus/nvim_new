de8bd50ef3aaa83e24c59d2baa44d0155b3ab94f
Angus Chang <changangus2@gmail.com>
2023-06-14 14:32:36 -0400

    refactor: remove unneccessary arg

--- after/plugin/gh_pulls.lua
+++ after/plugin/gh_pulls.lua
@@ -5,4 +5,4 @@ gh_plugin.setup({
 })
 
 vim.keymap.set("n", "<leader>pr", ":lua require('github_pulls.ui').toggle_pr_menu()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>pR", ":lua require('github_pulls.ui').toggle_reviews_menu(true)<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>pR", ":lua require('github_pulls.ui').toggle_reviews_menu()<CR>", { noremap = true, silent = true })
