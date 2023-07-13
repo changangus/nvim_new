ca232af9e6bece37a3f43667f63408c44e79a7f5
Angus Chang <changangus2@gmail.com>
2023-07-13 10:47:49 -0400

    feat: graphql lsp support

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('i','<C-j>', 'copilot#Accept("<CR>")', { expr=true, silent=true })
 vim.api.nvim_set_keymap('i','<C-c>', ':Copilot panel', { expr=true, silent=true })
+vim.g.copilot_no_tab_map = true
