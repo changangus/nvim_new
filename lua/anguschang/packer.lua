ec53d701308a19a432776924356ce056e37a156c
Angus Chang <changangus2@gmail.com>
2023-07-27 20:12:57 -0400

    feat: nx

--- lua/anguschang/packer.lua
+++ lua/anguschang/packer.lua
@@ -87,4 +87,14 @@ return require('packer').startup(function(use)
   end }
   -- Emmet 
   use { 'mattn/emmet-vim' }
+  use {
+    "ceuk/nx.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>nxa", ":Telescope nx actions<CR>", { desc = "NX Actions" })
+      vim.keymap.set("n", "<leader>nxr", require("nx").setup, { desc = "NX init/reload" })
+    end
+  }
 end)
