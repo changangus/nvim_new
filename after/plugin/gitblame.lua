2a330e2c9c15c40fdd570e1cf0a12bfa7e7d8055
Angus Chang <changangus2@gmail.com>
2023-11-30 09:54:43 -0500

    feat: disable git blame by default

--- after/plugin/gitblame.lua
+++ after/plugin/gitblame.lua
@@ -1,4 +1,7 @@
 -- GitBlame
+require('gitblame').setup {
+  enabled = false,
+}
 vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<CR>', { noremap = true });
 vim.keymap.set('n', '<leader>gO', ':GitBlameOpenCommitURL<CR>', { noremap = true });
 vim.keymap.set('n', '<leader>gS', ':GitBlameCopySHA<CR>', { noremap = true });
